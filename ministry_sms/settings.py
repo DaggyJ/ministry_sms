9965231c95243a352e89917cad550751dc097f2b
DaggyJ <tranxporterdaggy@gmail.com>
2025-12-05 19:42:23 +0300

    Created Requirements

--- ministry_sms/settings.py
+++ ministry_sms/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1-a=j&3aw6ota(&!187ilrkk8qjx0@z5-u+g!794m@2cas7v%%'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
